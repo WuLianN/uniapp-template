76389ef81da7fa999e5c239f6eeeb2ab6374bd27
WuLianN <610575024@qq.com>
2023-02-12 11:59:25 +0800

    chore: 配置tabBar

---
+++ src/pages/menu/menu.vue
@@ -0,0 +1,7 @@
+<template>
+  <view>菜单</view>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
---
+++ src/pages/my/my.vue
@@ -0,0 +1,7 @@
+<template>
+  <view>我的</view>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
