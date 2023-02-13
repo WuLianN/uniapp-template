fbcd9f2fc32320335b68e9c61b520c8ff723dc6b
WuLianN <610575024@qq.com>
2023-02-13 22:52:51 +0800

    chore: 配置代理

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
-
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
@@ -9,6 +8,14 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
     }
   }
 })
