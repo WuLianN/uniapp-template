d671a2af33ac6b229343f296d9d1117adcb8d4d9
WuLianN <610575024@qq.com>
2023-02-24 23:04:15 +0800

    chore: 添加vite-plugin-mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,31 @@
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 import { resolve } from 'path'
+import { viteMockServe } from 'vite-plugin-mock'
+
+console.info(resolve(__dirname, 'src/mock'))
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [uni()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      uni(),
+      viteMockServe({
+        mockPath: 'src/mock',
+        localEnabled: command === 'serve'
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true
+        }
       }
     }
   }
